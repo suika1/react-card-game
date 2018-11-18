30ca0d4a492e5a8724431a9ae8d1da3f66f3e022
suika1 <vanyoktank1@mail.ru>
2018-11-18 21:35:42 +0300

    Updates

---
+++ precache-manifest.75a44179cea8f5961bb86530ab494997.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "62a3b333eae040348e75f0f511c1bc3c",
+    "url": "/react-card-game/static/media/6-13.62a3b333.jpg"
+  },
+  {
+    "revision": "76da43852214d4f85638",
+    "url": "/react-card-game/static/css/main.6df7bc63.chunk.css"
+  },
+  {
+    "revision": "3795073cdb64ffa9d4a7",
+    "url": "/react-card-game/static/js/1.3795073c.chunk.js"
+  },
+  {
+    "revision": "b1798a4ef4699f6719ed",
+    "url": "/react-card-game/static/js/runtime~main.b1798a4e.js"
+  },
+  {
+    "revision": "29c581ceca4687fa286a936a16031d8e",
+    "url": "/react-card-game/static/media/4-1.29c581ce.jpg"
+  },
+  {
+    "revision": "5186fe5eba9d90de3416067375480152",
+    "url": "/react-card-game/static/media/4-2.5186fe5e.jpg"
+  },
+  {
+    "revision": "2f532660f35f4e57f23720ec7ec0d73e",
+    "url": "/react-card-game/static/media/4-3.2f532660.jpg"
+  },
+  {
+    "revision": "b8218b268f8e96cbeb952bc8a5c392e7",
+    "url": "/react-card-game/static/media/4-4.b8218b26.jpg"
+  },
+  {
+    "revision": "94bbaea60ee302d6d676e12fd071b2a4",
+    "url": "/react-card-game/static/media/4-5.94bbaea6.jpg"
+  },
+  {
+    "revision": "d69626da60f8d8ed533086647c21803b",
+    "url": "/react-card-game/static/media/4-6.d69626da.png"
+  },
+  {
+    "revision": "db1c1bdeb0192f478f12c6d8e2bb4648",
+    "url": "/react-card-game/static/media/4-7.db1c1bde.png"
+  },
+  {
+    "revision": "4b752f3604c9e2ff4414372295e34270",
+    "url": "/react-card-game/static/media/4-8.4b752f36.jpg"
+  },
+  {
+    "revision": "535944ccbcee2f1fe387b4c974cc2aef",
+    "url": "/react-card-game/static/media/6-1.535944cc.jpg"
+  },
+  {
+    "revision": "c92afffc348cf5721bfa7819cf765e70",
+    "url": "/react-card-game/static/media/6-10.c92afffc.jpg"
+  },
+  {
+    "revision": "44cbcedb7e2304903b11c3b2a649adc3",
+    "url": "/react-card-game/static/media/6-11.44cbcedb.jpg"
+  },
+  {
+    "revision": "5ad1023b24e4de693ab5525fa62e9c0a",
+    "url": "/react-card-game/static/media/6-12.5ad1023b.jpg"
+  },
+  {
+    "revision": "76da43852214d4f85638",
+    "url": "/react-card-game/static/js/main.76da4385.chunk.js"
+  },
+  {
+    "revision": "d497370344978e945bac8418fe510c14",
+    "url": "/react-card-game/static/media/6-14.d4973703.jpg"
+  },
+  {
+    "revision": "460c05a396f063a621e8e683a5413f7a",
+    "url": "/react-card-game/static/media/6-15.460c05a3.jpg"
+  },
+  {
+    "revision": "a5d4df7fadceb34c8cf3a57e5b8ba18d",
+    "url": "/react-card-game/static/media/6-16.a5d4df7f.jpg"
+  },
+  {
+    "revision": "43aed17aac725277896f06fe04ed201f",
+    "url": "/react-card-game/static/media/6-17.43aed17a.jpg"
+  },
+  {
+    "revision": "08c9b06f60c5c9542b5d3f8bbb184e6d",
+    "url": "/react-card-game/static/media/6-18.08c9b06f.jpg"
+  },
+  {
+    "revision": "9224202ddc2eba8cde68f9141a8fa23b",
+    "url": "/react-card-game/static/media/6-2.9224202d.jpg"
+  },
+  {
+    "revision": "bd8b84905253e424714b64a6fdb11ca0",
+    "url": "/react-card-game/static/media/6-3.bd8b8490.jpg"
+  },
+  {
+    "revision": "f2bcf17cdb2da67514c97e607dd1bd72",
+    "url": "/react-card-game/static/media/6-4.f2bcf17c.jpg"
+  },
+  {
+    "revision": "20eeb5d257d0a0d9a0965bd61c3b91fe",
+    "url": "/react-card-game/static/media/6-5.20eeb5d2.jpg"
+  },
+  {
+    "revision": "ce2ef02ee48e840820109b81e85da58b",
+    "url": "/react-card-game/static/media/6-6.ce2ef02e.jpg"
+  },
+  {
+    "revision": "d48f8c2cec7c4cb4b76f870cf1f58e2b",
+    "url": "/react-card-game/static/media/6-7.d48f8c2c.jpg"
+  },
+  {
+    "revision": "50f397706e676a8fc42c7db2b54662a9",
+    "url": "/react-card-game/static/media/6-8.50f39770.jpg"
+  },
+  {
+    "revision": "2c355cfeebea49906927ca6b460c5ad9",
+    "url": "/react-card-game/static/media/6-9.2c355cfe.jpg"
+  },
+  {
+    "revision": "9acb110b6723b5728c0dafa3f0ab3389",
+    "url": "/react-card-game/static/media/s1.9acb110b.png"
+  },
+  {
+    "revision": "f75440f9fe6f86273d491ceca59b498b",
+    "url": "/react-card-game/static/media/s2.f75440f9.png"
+  },
+  {
+    "revision": "7efbb050612ea3c81af60d745a310554",
+    "url": "/react-card-game/index.html"
+  }
+];
