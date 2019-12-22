0e758f1109afe7aeb8663831ee6e5229b726f56f
Ivan <30298363+suika1@users.noreply.github.com>
2019-12-22 21:09:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ class Card extends React.Component{
         let imageUrl;
         let style = {};
         let shirtUrl;
+        return 'test of jenkins';
 
         if (this.props.img !== null) {
             imageUrl = require(`./img/${this.props.img}`);
